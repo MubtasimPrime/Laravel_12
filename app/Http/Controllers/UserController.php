096aa2e886a7f499bf323e89163fdeb9ca87a5de
MubtasimPrime <mubtasimfuadrafiq@gmail.com>
2025-05-15 15:41:20 +0600

    Update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,12 +7,23 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     //
-    function getUser(){
-        return "Code step by step";
+    function getUser()
+    {
+        // return "Code step by step";
+        return view('user');
     }
-    function aboutUser(){
+    function aboutUser()
+    {
         return "Code about";
     }
+    function getUserName($name)
+    {
+        return "Hello this is " . $name;
+    }
+    function userHome()
+    {
+        $name = 'Anil';
+        $user = ['anil', 'sam', 'peter'];
+        return view('home', ['name' => $name, 'user' => $user]);
+    }
 }
-
-
--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', fn()=> view('home'));
+Route::get('/home', fn() => view('home'));
 
 // Route::view('/home','home');
 
-Route::get('/about/{name}', function ($name) {
-    return view('about',['name'=>$name]);
-});
+// Route::get('/about/{name}', function ($name) {
+//     return view('about',['name'=>$name]);
+// });
 
 Route::get('home', [UserController::class, 'getUser']);
 Route::get('about', [UserController::class, 'aboutUser']);
+Route::get('about/{name}', [UserController::class, 'getUserName']);
